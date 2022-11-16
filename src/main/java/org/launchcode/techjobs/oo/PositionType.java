091c3c42bd347153c438a45aa8dbf76f035621da
Raniya Abdalla <58631480+raniyaabdalla@users.noreply.github.com>
2022-11-15 23:23:37 -0500

    Part1&2

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -7,17 +7,18 @@ public class PositionType {
     private int id;
     private static int nextId = 1;
     private String value;
-    @Override
-    public boolean equals(Object o){
-        if (this == o) return true;
-        if (o == null || getClass()!= o.getClass()) return false;
-        PositionType that = (PositionType) o;
-        return id == that.id;
-    }
-    @Override
-    public int hashCode(){
 
-        return Objects.hash(id);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
     }
 
     public PositionType() {
